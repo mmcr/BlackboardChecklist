b910a6699e299b995f710bf6913a719f895ed758
Matthew Roberts <thew.roberts@gmail.com>
2020-10-06 13:28:03 +0000

    Moved code around

--- bbchecklist.js
+++ bbchecklist.js
@@ -4,40 +4,38 @@
 	}
 	
 	let editingStatusElement = document.getElementById('statusText')
-	let courseID = document.querySelector('li.root.coursePath a').getAttribute('href').match(/[\w_]+$/)[0]
-	let userID = document.getElementById('magicUserID').innerText.trim()
-		
-		
-		
-	const createCheckboxFromListItem = function(el) {
-		let key = `${userID}-${courseID}-${pageID}-` + el.innerText.slice(0,20).replaceAll(" ", "")
-		el.innerHTML = `<input type="checkbox" style="margin-right:1rem;"/>${el.innerHTML}`
-		let check = el.down('input')
-		el.style.display = "flex"
-		el.style.alignItems = "center"
-		el.style.marginLeft = "-2rem"
-	
-		check.on('click', x => {
-			if (check.checked) {
-				el.style.color = "gray"
-				el.style.textDecoration = "line-through"
-				el.style.textDecorationColor = "red"
-				localStorage.setItem(key, true)
-			} else {
-				el.style.color = "black"
-				el.style.textDecoration = "none"
-				localStorage.setItem(key, false)
-			}
-		})
-		if (localStorage.getItem(key) == "true") {
-			check.click()
-		}
-	
-	}
 	
 	if ((editingStatusElement == null) || (editingStatusElement.innerText == "OFF")) {
-		let pageID = document.querySelector('#pageTitleDiv form').getAttribute('action').match(/content_id=([\w_]+)&/)[1]
+		let courseID = document.querySelector('li.root.coursePath a').getAttribute('href').match(/[\w_]+$/)[0]
+		let userID = document.getElementById('magicUserID').innerText.trim()
+		let pageID = document.querySelector('#pageTitleDiv form').getAttribute('action').match(/content_id=([\w_]+)&/)[1]		
+
+		const createCheckboxFromListItem = function(el) {
+			let key = `${userID}-${courseID}-${pageID}-` + el.innerText.slice(0,20).replaceAll(" ", "")
+			el.innerHTML = `<input type="checkbox" style="margin-right:1rem;"/>${el.innerHTML}`
+			let check = el.down('input')
+			el.style.display = "flex"
+			el.style.alignItems = "center"
+			el.style.marginLeft = "-2rem"
 		
+			check.on('click', x => {
+				if (check.checked) {
+					el.style.color = "gray"
+					el.style.textDecoration = "line-through"
+					el.style.textDecorationColor = "red"
+					localStorage.setItem(key, true)
+				} else {
+					el.style.color = "black"
+					el.style.textDecoration = "none"
+					localStorage.setItem(key, false)
+				}
+			})
+			if (localStorage.getItem(key) == "true") {
+				check.click()
+			}
+		
+		}
+
 		Array.from(document.getElementsByClassName('liItem read'))
 			.filter(contentItem =>  ['Learner Guide'].includes(contentItem.querySelector('h3').innerText))
 			.map(contentItem => contentItem.querySelector(".vtbegenerated").children)
