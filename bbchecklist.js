76618d2277cf50d29c434fd10f0612c94d7d7312
Matthew Roberts <thew.roberts@gmail.com>
2020-10-06 13:02:59 +0000

    Added courseID to key and switched from page to content ID

--- bbchecklist.js
+++ bbchecklist.js
@@ -1,48 +1,55 @@
-const createCheckboxFromListItem = function(el) {
-	let key = pageTitle + "-" + el.innerText.slice(0,15).replaceAll(" ", "")
-	el.innerHTML = `<input type="checkbox" style="margin-right:1rem;"/>${el.innerHTML}`
-	let check = el.down('input')
-	el.style.display = "flex"
-	el.style.alignItems = "center"
-	el.style.marginLeft = "-2rem"
-
-	check.on('click', x => {
-		if (check.checked) {
-			el.style.color = "gray"
-			el.style.textDecoration = "line-through"
-			el.style.textDecorationColor = "red"
-			localStorage.setItem(key, true)
-		} else {
-			el.style.color = "black"
-			el.style.textDecoration = "none"
-			localStorage.setItem(key, false)
+(() => {
+	String.prototype.scrub = function() {
+			return this.replaceAll(/[^\w]/g, "")
 		}
-	})
-	if (localStorage.getItem(key) == "true") {
-		check.click()
+	
+	let editingStatusElement = document.getElementById('statusText')
+	let courseID = document.querySelector('li.root.coursePath a').getAttribute('href').match(/[\w_]+$/)[0]
+	let pageID = document.querySelector('#pageTitleDiv form').getAttribute('action').match(/content_id=([\w_]+)&/)[1]
+
+		
+		
+		
+	const createCheckboxFromListItem = function(el) {
+		let key = `${courseID}-${pageID}-` + el.innerText.slice(0,15).replaceAll(" ", "")
+		el.innerHTML = `<input type="checkbox" style="margin-right:1rem;"/>${el.innerHTML}`
+		let check = el.down('input')
+		el.style.display = "flex"
+		el.style.alignItems = "center"
+		el.style.marginLeft = "-2rem"
+	
+		check.on('click', x => {
+			if (check.checked) {
+				el.style.color = "gray"
+				el.style.textDecoration = "line-through"
+				el.style.textDecorationColor = "red"
+				localStorage.setItem(key, true)
+			} else {
+				el.style.color = "black"
+				el.style.textDecoration = "none"
+				localStorage.setItem(key, false)
+			}
+		})
+		if (localStorage.getItem(key) == "true") {
+			check.click()
+		}
+	
 	}
-
-}
-String.prototype.scrub = function() {
-	return this.replaceAll(/[^\w]/g, "")
-}
-
-
-let editingStatusElement = document.getElementById('statusText')
-let pageTitle = $('pageTitleText').innerText.scrub()
-
-if ((editingStatusElement == null) || (editingStatusElement.innerText == "OFF")) {
-	Array.from(document.getElementsByClassName('liItem read'))
-		.filter(contentItem => contentItem.querySelector('h3').innerText == "Learner Guide")
-		.map(contentItem => contentItem.querySelector(".vtbegenerated").children)
-		.map(elements => Array.from(elements))
-		.flat()
-		.filter(tag => tag.tagName == "UL")
-		.map(tag => Array.from(tag.children))
-		.flat()
-		.forEach(listItem => createCheckboxFromListItem(listItem))
-	Array.from(document.getElementsByClassName('liItem read'))
-		.filter(contentItem => contentItem.querySelector('h3').innerText == "Checklist Script")
-		.forEach(contentItem => contentItem.style.display = "none")
-
-}
+	
+	if ((editingStatusElement == null) || (editingStatusElement.innerText == "OFF")) {
+		Array.from(document.getElementsByClassName('liItem read'))
+			.filter(contentItem =>  ['Learner Guide'].includes(contentItem.querySelector('h3').innerText))
+			.map(contentItem => contentItem.querySelector(".vtbegenerated").children)
+			.map(elements => Array.from(elements))
+			.flat()
+			.filter(tag => tag.tagName == "UL")
+			.map(tag => Array.from(tag.children))
+			.flat()
+			.forEach(listItem => createCheckboxFromListItem(listItem))
+		Array.from(document.getElementsByClassName('liItem read'))
+			.filter(contentItem => contentItem.querySelector('h3').innerText == "Checklist Script")
+			.forEach(contentItem => contentItem.style.display = "none")
+	
+	}
+	
+})()
