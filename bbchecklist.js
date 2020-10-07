1586f2a7f2cd6acb4f7136d0426a5fb5a9cd28d9
Matthew Roberts <thew.roberts@gmail.com>
2020-10-07 00:51:12 +0000

    Troubleshooting

--- bbchecklist.js
+++ bbchecklist.js
@@ -1,4 +1,6 @@
 (() => {
+	console.log("at the start")
+
 	let head = document.getElementsByTagName("head")[0]
 
 	String.prototype.scrub = function() {
@@ -10,10 +12,10 @@
 	if ((editingStatusElement == null) || (editingStatusElement.innerText == "OFF")) {
 		[{
 			"id": 		"FirebaseApp",
-			"src": 		"https://www.gstatic.com/firebasejs/7.22.1/firebase-app.js"
+			"src": 		"https://www.gstatic.com/firebasejs/7.18.0/firebase-app.js"
 		}, {
 			"id": 		"FirebaseDatabase",
-			"src": 		"https://www.gstatic.com/firebasejs/7.22.1/firebase-database.js"
+			"src": 		"https://www.gstatic.com/firebasejs/7.18.0/firebase-database.js"
 		}].forEach(x => {
 			if (!document.getElementById(x.id)) {
 				let link = document.createElement('script')
@@ -24,41 +26,47 @@
 		let database
 		let checkboxData = null
 		const saveCheckboxToDatabase = function(checkboxID, value) {
-			let ref = database.ref().child(`${userID}/${courseID}/${pageID}/${checkboxID}`)
+			let ref = database.ref().child(`checkboxes/${userID}/${courseID}/${pageID}/${checkboxID}`)
 			ref.set(value)
 				.then(() => console.log("Saved checkbox"))
 				.catch(error => console.error(`Error saving checkbox: ${error}`))
 		}
 		const loadCheckboxesFromDatabase = function() {
-			let ref = database.ref().child(`${userID}/${courseID}/${pageID}`)
+			let ref = database.ref().child(`checkboxes/${userID}/${courseID}/${pageID}`)
 
-			let valuePromise = new Promise((resolve, reject) => {
-				ref.once("value", function(snapshot) {
-					if (snapshot.numChildren() > 0) {
-						checkboxData = new Map(Object.entries(snapshot.val()))
-						resolve()
-					}
-				}, function (errorObject) {
-					reject(errorObject)
-				})
-			})
+// 			let valuePromise = new Promise((resolve, reject) => {
+// 				ref.once("value", function(snapshot) {
+// 					if (snapshot.numChildren() > 0) {
+// 						checkboxData = new Map(Object.entries(snapshot.val()))
+// 					}
+// 					resolve()
+// 
+// 				}, function (errorObject) {
+// 					reject(errorObject)
+// 				})
+// 			})
 				
 			
 			
 			
 			
-			// let valuePromise = ref.once("value")
-			// 	.then(snapshot => {
-			// 		if (snapshot.numChildren() > 0) {
-			// 			checkboxData = new Map(Object.entries(snapshot.val()))
-			// 		}
-			// 	})
-			// 	.catch(error => console.error(`Unable to load checkboxes: ${error}`))
-			ref.on('child_added', function(childSnapshot) {				
-				console.log(childSnapshot)	
-			})
-			ref.once('child_changed', function(childSnapshot) {
-				console.log(childSnapshot)	
+			let valuePromise = ref.once("value")
+				.then(snapshot => {
+					if (snapshot.numChildren() > 0) {
+						checkboxData = new Map(Object.entries(snapshot.val()))
+					}
+				})
+				.catch(error => console.error(`Unable to load checkboxes: ${error}`))
+			
+			// ref.on('child_added', function(childSnapshot) {
+			// 	console.log(`added: ${childSnapshot}`)	
+			// }, function(error) {
+			// 	console.log(`Error: ${error}`)
+			// })
+			ref.on('child_changed', function(childSnapshot) {
+				console.log(`changed: ${childSnapshot}`)	
+			}, function(error) {
+				console.log(`Error: ${error}`)
 			})
 			return valuePromise
 		}
@@ -67,6 +75,7 @@
 		let courseID = document.querySelector('li.root.coursePath a').getAttribute('href').match(/[\w_]+$/)[0]
 		let userID = document.getElementById('magicUserID')?.innerText.trim() ?? "DEBUG"
 		let pageID = document.querySelector('#pageTitleDiv form').getAttribute('action').match(/content_id=([\w_]+)&/)[1]
+		let appInstance
 
 		const changeCheckboxStyles = function(el, checked) {
 			if (checked) {
@@ -115,18 +124,23 @@
 		}
 
 		const makeTheMagicHappen = function() {
-			firebase.initializeApp({
-				apiKey: "AIzaSyAHe2hquz7tuauLQKEXmK44s3jl02gdzl4",
-				authDomain: "bbchecklist-c599c.firebaseapp.com",
-				databaseURL: "https://bbchecklist-c599c.firebaseio.com",
-				projectId: "bbchecklist-c599c",
-				storageBucket: "bbchecklist-c599c.appspot.com",
-				messagingSenderId: "1025910668427",
-				appId: "1:1025910668427:web:69615a1ebcdc895df1288e"
-			})
+			console.log("in makeTheMagicHappen")
+			if (appInstance == null) {
+				console.log("initing Firebase")
+				appInstance = firebase.initializeApp({
+					apiKey: "AIzaSyAHe2hquz7tuauLQKEXmK44s3jl02gdzl4",
+					authDomain: "bbchecklist-c599c.firebaseapp.com",
+					databaseURL: "https://bbchecklist-c599c.firebaseio.com",
+					projectId: "bbchecklist-c599c",
+					storageBucket: "bbchecklist-c599c.appspot.com",
+					messagingSenderId: "1025910668427",
+					appId: "1:1025910668427:web:69615a1ebcdc895df1288e"
+				})
+			}
 			database = firebase.database()
 			loadCheckboxesFromDatabase()
 				.then(() => {
+					console.log("before building checkboxes")
 					Array.from(document.getElementsByClassName('liItem read'))
 						.filter(contentItem =>  ['Learner Guide'].includes(contentItem.querySelector('h3').innerText))
 						.map(contentItem => contentItem.querySelector(".vtbegenerated").children)
@@ -141,7 +155,7 @@
 						.forEach(contentItem => contentItem.style.display = "none")
 				})
 		}
-		window.setTimeout(makeTheMagicHappen, 150)
+		window.setTimeout(makeTheMagicHappen, 250)
 	}
 	
 })()
